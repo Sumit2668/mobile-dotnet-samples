5ed45c200385d89f12fe310a5dd43bb559747b66
amay077 <okuokuoku@gmail.com>
2016-12-18 19:44:13 +0900

    Change data source to https://mierune2016team.carto.com/viz/8a9450a2-6456-11e6-8a7a-0ee66e2c9693/public_map

--- CartoMap.iOS/Sections/CARTO.js API/CountriesVisController.cs
+++ CartoMap.iOS/Sections/CARTO.js API/CountriesVisController.cs
@@ -11,7 +11,8 @@ namespace CartoMap.iOS
 		{
 			get
 			{
-				return "http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json";
+//				return "http://documentation.cartodb.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json";
+				return "https://mierune2016team.carto.com/api/v2/viz/8a9450a2-6456-11e6-8a7a-0ee66e2c9693/viz.json";
 			}
 		}
 	}
